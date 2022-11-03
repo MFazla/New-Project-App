d71c78bbeb4a55ee743594d1c240100b1510f8c4
MFazla <2019t00441@stu.cmb.ac.lk>
2022-11-03 10:34:00 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Color.fromARGB(255, 187, 245, 121),
+      body: Center(
+        child: Image.asset('assets/Images/nature.jpg'),
+      ),
+    ),
+  ));
+}
